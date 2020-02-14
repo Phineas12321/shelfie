72fc09b43c4c4d0bc1adf64057cf868569c522ee
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-14 10:50:22 -0700

    step one done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,56 @@
 import React from 'react'
+import './form.css'
 
 class Form extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
-
+            imageUrl: '',
+            productName: '',
+            price: 0
         }
+
+        this.handleImageUrl = this.handleImageUrl.bind(this)
+        this.handleProductName = this.handleProductName.bind(this)
+        this.handlePrice = this.handlePrice.bind(this)
+        this.handleReset = this.handleReset.bind(this)
+    }
+
+    handleImageUrl(e){
+        this.setState({imageUrl: e.target.value})
+    }
+
+    handleProductName(e){
+        this.setState({productName: e.target.value})
+    }
+
+    handlePrice(e){
+        this.setState({price: e.target.value})
+    }
+
+    handleReset(){
+        this.setState({
+            imageUrl: '',
+            productName: '',
+            price: 0
+        })
     }
 
     render(){
         return(
-            <div>
-                Form
+            <div className='form-box'>
+                Image URL:
+                <input onChange={this.handleImageUrl} />
+                Product Name:
+                <input onChange={this.handleProductName} />
+                Price:
+                <input onChange={this.handlePrice} />
+                <div className='form-buttons'>
+                    <button onClick={this.handleReset} >Cancel</button>
+                    <button>Add to Inventory</button> 
+                </div>
+                
             </div>
         )
     }
